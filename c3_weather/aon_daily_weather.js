af91103cbc6ad6b34d9eac58f79852b1cf9186ed
Sarah Cnota <scnota@gmail.com>
2015-08-23 11:02:21 -0500

    Add precipitation to graph, customize axes and legend

--- c3_weather/aon_daily_weather.js
+++ c3_weather/aon_daily_weather.js
@@ -5,23 +5,69 @@ var chart = c3.generate({
     mimeType: 'json',
     keys: {
       x: 'time',
-      value: ['temperatureMax', 'temperatureMin'],
+      value: ['temperatureMax', 'temperatureMin','precipIntensityMax'],
+    },
+    names: {
+      temperatureMin: "Temperature Low",
+      temperatureMax: "Temperature High",
+      precipIntensityMax: "Precipitation Max"
+    },
+    axes: {
+      temperatureMax: 'y',
+      precipIntensityMax: 'y2'
+    },
+    types: {
+      temperatureMin: "spline",
+      temperatureMax: "spline",
+      precipIntensityMax: "bar"
+    },
+    colors: {
+      temperatureMin: '#e0301e',
+      temperatureMax: '#602320',
+      precipIntensityMax: '#eb8c00',
     }
   },
   axis: {
     x: {
-       type: 'timeseries',
-       tick: {
-         format: '%A'
-         //format: function (v) { return formatTimeAsDate(v); },
-       }
+      type: 'timeseries',
+      tick: {
+        format: '%A'
+      },
+      label: {
+        text: 'Day of Week',
+        position: 'outer-center'
+      }
+    },
+    y: {
+      max: 100,
+      min: 30,
+      label: {
+        text: 'Temperature (Fahrenheit)',
+          position: 'outer-middle'
+        }
+    },
+    y2: {
+      show: true,
+      max: 0.5,
+      label: {
+        text: 'Precipitation (Inches)',
+          position: 'outer-middle'
+        }
     }
   },
   padding: {
-    top: 100
+    top: 80
   },
   size: {
     height: 400
+  },
+  legend: {
+    position: 'inset',
+    inset: {
+      anchor: 'top-right',
+      x: 80,
+      y: -50,
+    }
   },
   oninit: function () {
     d3.select("svg").append("text")
@@ -32,9 +78,3 @@ var chart = c3.generate({
     }
 });
 
-
-var formatTimeAsDate = function(time) {
-  var date = new Date(0);
-  date.setUTCSeconds(time);
-  return date.toLocaleDateString();
-}
